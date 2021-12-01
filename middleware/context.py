258bdd2ec525237f848c25bfb61df29fc20cb4de
Daktor99 <ed2918@columbia.edu>
2021-12-01 18:31:26 -0500

    Deletion of files and basic skeleton

--- middleware/context.py
+++ middleware/context.py
@@ -30,3 +30,15 @@ def get_db_info():
         }
 
     return db_info
+
+def get_user_service_url():
+
+    return os.environ.get("COMP_USER_HOST", None)
+
+def get_movie_service_url():
+
+    return os.environ.get("COMP_MOVIE_HOST", None)
+
+def get_recommendation_service_url():
+
+    return os.environ.get("COMP_REC_HOST", None)
