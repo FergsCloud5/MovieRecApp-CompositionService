e469b03285bb380c01b2bb1d7481a27a38861c39
Daktor99 <ed2918@columbia.edu>
2021-12-04 18:08:45 -0500

    Finished parallel reccomendation posting

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import json
+from datetime import datetime
+
+import asyncio
 from flask import Flask, Response, request
 from flask_cors import CORS
 import logging
@@ -13,7 +17,7 @@ CORS(app)
 
 ##################################################################################################################
 
-def updateRecommendation(recommendation):
+async def updateRecommendation(recommendation):
 
     userID = recommendation["userID"]
     movieID = recommendation["movieID"]
@@ -33,6 +37,7 @@ def updateRecommendation(recommendation):
         response = requests.post(postUrl, json=recommendation)
         if response.status_code != 201:
             logger.error("Error with /recommendations POST: ", response.text)
+        return response
     else:
         # otherwise, increment the count of the recommendation and use put to update recommendation
         recommendation = recommendationCheck.json()["data"][0]
@@ -41,7 +46,7 @@ def updateRecommendation(recommendation):
         response = requests.put(putUrl, json=recommendation)
         if response.status_code != 200:
             logger.error("Error with /recommendations/userID/movieID PUT: ", response.text)
-
+        return response
 
 ##################################################################################################################
 
@@ -56,49 +61,55 @@ def userSeqPost():
 
 ##### Remove GET later! ######
 @app.route('/movieHistoryParallelPost', methods=["POST"])
-def userParallelPost():
+async def userParallelPost():
+    try:
+        # trying to post a user movie history, first check if movie exists in DB
+        body = request.get_json()
+        userID = body.get("userID", None)
+        movieID = body.get("movieID", None)
+        if not userID or not movieID:
+            return Response("Must provide userID and movieID", status=400)
 
-    # trying to post a user movie history, first check if movie exists in DB
-    body = request.get_json()
+        # check if this movie exists in DB
+        movieQuery = "movieID=" + movieID
+        movieUrl = context.get_movie_service_url() + "/movies?movie_id=" + movieID
+        movieCheck = requests.get(movieUrl)
+        movieStatus = movieCheck.status_code
+        movieCheck = movieCheck.json()
+        if len(movieCheck) == 0 or movieStatus != 200:
+            return Response("Movie does not exist.", status=404)
 
-    # # # TODO: Check input and assert userID and movieID are present, otherwise cannot post
-    # movieID = body["movieID"]
-    # movieQuery = "movieID=" + movieID
-    # movieUrl = context.get_movie_service_url() + "/movies?movie_id=" + movieID
-    # movieCheck = requests.get(movieUrl).json()
-    # # if len(movieCheck) == 0:
-    # #     # raise error, movie does not exist
-    #
-    # # if we get here, movie exists, generate its recommendations
-    # getRecUrl = context.get_recommendation_service_url() + "/similarity?" + movieQuery
-    # postRecUrl = context.get_recommendation_service_url() + "/recommendations"
-    # recommendations = requests.get(getRecUrl).json()
-    # recommendations = recommendations["recommendations"]
-    #
-    # # for each recommendation, in parallel, either insert record or increment count
-    #
-    #
-    # rec = recommendations[0]
-    # print(recommendations)
+        # construct movie history and make insertion into userMovieHistory first
+        historyUrl = context.get_user_service_url() + "/movie-histories"
+        movieHistory = {"userID": userID,
+                        "movieID": movieID,
+                        "movieTitle": movieCheck[0]["movie_title"],
+                        "likedMovie": body.get("likedMovie", 0)}
+        historyPost = requests.post(historyUrl, json=movieHistory)
+        if historyPost.status_code != 201:
+            return Response(historyPost, content_type="application/json")
 
-    rec = {"userID": body["userID"],
-           "movieID": body["movieID"]}
-    updateRecommendation(rec)
+        # if we get here, movie exists and was posted, generate its recommendations only if user liked movie
+        if movieHistory["likedMovie"] == 1:
+            getRecUrl = context.get_recommendation_service_url() + "/similarity?" + movieQuery
+            recommendations = requests.get(getRecUrl).json()
+            recommendations = recommendations["recommendations"]
+            # for each recommendation, in parallel, either insert record or increment count
+            rs = (updateRecommendation({"userID": body["userID"], "movieID": recommendation["movieID"]}) for recommendation in recommendations)
+            L = await asyncio.gather(*rs)
+            print(L)
 
-    return "This will do the parallel post"
+        # if all succeeds, return regular response for user movie history post, so service works like before
+        # return Response(historyPost.json(), status=historyPost.status_code, content_type="application/json")
+        return Response(historyPost, content_type="application/json")
 
-# These three are just temporary: to be deleted later
-@app.route('/userUrl')
-def getUserUrl():
-    return str(context.get_user_service_url())
-
-@app.route('/movieUrl')
-def getMovieUrl():
-    return str(context.get_movie_service_url())
-
-@app.route('/recUrl')
-def getRecUrl():
-    return str(context.get_recommendation_service_url())
+    except Exception as e:
+        statusRespDict = {
+            "status": "500 Internal server error",
+            "error message": str(e)
+        }
+        rsp = Response(json.dumps(statusRespDict), status=500, content_type='application/json')
+        return rsp
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
